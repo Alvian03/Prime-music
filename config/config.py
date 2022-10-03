4d8bf7472429628465b26107395353619deb42ed
vian_ofc <74562450+Alvian03@users.noreply.github.com>
2022-10-03 15:37:47 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -37,10 +37,10 @@ SONG_DOWNLOAD_DURATION = int(
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001716892855"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001827529339"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","Zona Virtual Music Bot")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","Durex Music Bot")
 
 # Your User ID.
 OWNER_ID = list(
