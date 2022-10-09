2fd12aaef9778aeb1a26f09e4677be6a5cc13d1e
vian_ofc <74562450+Alvian03@users.noreply.github.com>
2022-10-09 19:14:23 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -73,7 +73,7 @@ SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/gausahkepoanjc"
 )  # Example:- https://t.me/gausahkepoanjc
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/famsyour_frend"
+    "SUPPORT_GROUP", ""
 )  # Example:- https://t.me/famsyour_frend
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
@@ -105,7 +105,7 @@ YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
 TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/")
+GITHUB_REPO = getenv("GITHUB_REPO", "")
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
